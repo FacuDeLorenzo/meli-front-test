658779e1dcc31749f9cd1fda9c166735942c37f5
Facundo De Lorenzo <facundo.delorenzo@gmail.com>
2023-02-14 17:21:43 -0300

    Removed unnecesary files + Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Products from "./Products";
+import Product from "./Product";
+import Search from "./Search";
+
+const App = () => {
+  const routes = [
+    <Route path="/" element={<Search />} />,
+    <Route path="/items" element={<Products />} />,
+    <Route path="/items/:id" element={<Product />} />,
+  ];
+  const router2 = createBrowserRouter(createRoutesFromElements(routes));
+  return <RouterProvider router={router2} />;
+};
 
 export default App;
