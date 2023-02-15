f35c680a3dbef3a0af2875e963b8f7f2119dc892
Facundo De Lorenzo <facundo.delorenzo@gmail.com>
2023-02-15 09:57:15 -0300

    Added react-helmet for SEO improvement

--- packages/api/src/items.controller.ts
+++ packages/api/src/items.controller.ts
@@ -3,17 +3,17 @@ import { ItemsService } from './items.service';
 
 @Controller('api/items')
 export class ItemsController {
-  constructor(private readonly appService: ItemsService) { }
+  constructor(private readonly itemsService: ItemsService) { }
 
   @Get()
   products(): string {
-    return this.appService.getProducts();
+    return this.itemsService.getProducts();
   }
   @Get(':id')
   product(@Param() params): string {
     if (params?.id) {
       return null;
     }
-    return this.appService.getProduct(params?.id);
+    return this.itemsService.getProduct(params?.id);
   }
 }
